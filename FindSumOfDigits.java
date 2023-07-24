e570b5cd0badd587a56e8e74b2a141dc127e08e5
techacademyhyd01 <ta.hyd.001@gmail.com>
2023-07-24 15:10:16 +0530

    probelm added

---
+++ FindSumOfDigits.java
@@ -0,0 +1,20 @@
+
+class FindSumOfDigits{
+
+public static int digitSum(int num){
+
+
+// Complete this method to find the sum of digits in num and return the sum
+
+}
+
+public static void main(String[] args)
+{
+
+   
+int sum = digitSum(567);
+System.out.println(sum);
+
+}
+
+}
